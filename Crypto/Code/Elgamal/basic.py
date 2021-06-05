4a51312b95d6383497e64214cb9bb749a3881f23
kenkenken2004 <kenfcw@outlook.jp>
2021-06-05 16:22:31 +0900

    Merge remote-tracking branch 'origin/main' into main

--- Crypto/Code/Elgamal/basic.py
+++ Crypto/Code/Elgamal/basic.py
@@ -3,20 +3,20 @@ from random import randrange as rand
 
 
 def key_gen(bits):
-    q = randprime((1 << (bits - 1)) + 1, 1 << bits)
-    g = rand(2, q)
-    x = rand(2, q)
-    h = pow(g, x, q)
-    return q, g, h, x
+    p = randprime((1 << (bits - 1)) + 1, 1 << bits)
+    g = rand(2, p)
+    x = rand(0, p)
+    h = pow(g, x, p)
+    return p, g, h, x
 
 
-def encrypt(plain, q, g, h):
-    r = rand(0, q)
-    c1 = pow(g, r, q)
-    c2 = plain * pow(h, r, q) % q
+def encrypt(plain, p, g, h):
+    r = rand(0, p)
+    c1 = pow(g, r, p)
+    c2 = plain * pow(h, r, p) % p
     return c1, c2
 
 
-def decrypt(cipher1, cipher2, q, x):
-    return cipher2 * pow(pow(cipher1, x, q), -1, q) % q
+def decrypt(cipher1, cipher2, p, x):
+    return cipher2 * pow(pow(cipher1, x, p), -1, p) % p
 
--- Crypto/Code/Elgamal/index.py
+++ Crypto/Code/Elgamal/index.py
@@ -5,15 +5,15 @@ def key_gen(bits):
     return basic.key_gen(bits)
 
 
-def encrypt(plain, q, g, h):
-    return basic.encrypt(plain, q, g, h)
+def encrypt(plain, p, g, h):
+    return basic.encrypt(plain, p, g, h)
 
 
-def decrypt(cipher1, cipher2, q, x):
-    return basic.decrypt(cipher1, cipher2, q, x)
+def decrypt(cipher1, cipher2, p, x):
+    return basic.decrypt(cipher1, cipher2, p, x)
 
 
 m = 39128581952325263237237231
-q, g, h, x = key_gen(1024)
-c1, c2 = encrypt(m, q, g, h)
-print(decrypt(c1, c2, q, x))
+p, g, h, x = key_gen(1024)
+c1, c2 = encrypt(m, p, g, h)
+print(decrypt(c1, c2, p, x))
