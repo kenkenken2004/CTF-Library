5040b517fe8dd342beeaec347435b5d0f99fb77d
Ken Fuchiwaki <kenfcw@outlook.jp>
2021-06-07 14:17:59 +0900

    Update basic.py

--- Crypto/Code/Paillier/basic.py
+++ Crypto/Code/Paillier/basic.py
@@ -1,1 +1,22 @@
-tet
+from sympy import randprime
+from random import randrange as rand
+
+def key_gen(bits):
+  p = randprime((1 << (bits//2 - 1)) + 1, 1 << (bits//2))
+  q = randprime((1 << (bits//2 - 1)) + 1, 1 << (bits//2))
+  n = p*q
+  k = rand(0,n)
+  g = (1 + k*n) % (n*n)
+  # pubkey:n,g  privatekey:p,q
+  return p, q, n, g
+
+
+def encrypt(m, n, g):
+  mod = n*n
+  r = rand(0,mod)
+  c = pow(g,m,mod) * pow(r,n,mod) % mod
+  return c
+
+
+def decrypt(c, p, q, n, g):
+  return 0
